e82add66fc2743d083f013e9bf3272f4a9e5c14e
Edward Zhu <edward.r.zhu@gmail.com>
2021-02-05 13:31:26 -0700

    parabolic sar updated to be class

--- algo_trading/strategies/parabolic_sar.py
+++ algo_trading/strategies/parabolic_sar.py
@@ -2,6 +2,33 @@ import time
 import talib
 import numpy as np
 
+class ParabolicSAR:
+    def __init__(self, weight, acceleration=0.02, maximum=0.2):
+        self.weight = weight
+        self.acceleration = acceleration
+        self.maximum = maximum
+
+    """Parabolic SAR implementation. Pass in highs and lows to return SAR points.
+    Args:
+        highs: A numpy array of floats representing high prices over a time period.
+        lows: A numpy array of floats representing low prices over a time period.
+        n: An integer (180, 360, or 720) representing the length of time series.
+    Returns:
+        A numpy array. Each element represents the parabolicSAR calculation
+    """
+    def calcParabolicSAR(self, highs, lows):
+        return talib.SAR(highs, lows, acceleration=self.acceleration, maximum=self.maximum)
+
+    def getScore(self, price, highs, lows):
+        sar = self.calcParabolicSAR(highs, lows)
+        
+        if price > sar[-1]:
+            return 1 * self.weight
+        elif price < sar[-1]:
+            return -1 * self.weight
+        return 0
+
+
 def parabolic_sar(highs, lows, acceleration=0.02, maximum=0.2):
     """Parabolic SAR implementation. Pass in highs and lows to return SAR points.
 
