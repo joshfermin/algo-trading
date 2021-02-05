0dcbd8bf7443db49d06a9c203e081f3e6c0fdd47
Edward Zhu <edward.r.zhu@gmail.com>
2021-02-05 13:31:08 -0700

    rsi updated into class

--- algo_trading/strategies/rsi.py
+++ algo_trading/strategies/rsi.py
@@ -2,6 +2,38 @@ import time
 import talib
 import numpy as np
 
+class RSI:
+    def __init__(self, weight, high=70, low=30):
+        self.weight = weight
+        self.high = high
+        self.low = low
+
+    """RSI implementation. Pass in prices to return RSI calulations.
+    Args:
+        prices: A numpy array of floats representing prices over a time period.
+    Returns:
+        A numpy array. Each element represents the parabolicSAR calculation
+    """
+    def calcRSI(self, prices):
+        return talib.RSI(prices)
+
+    def getScore(self, prices):
+        rsi = self.calcRSI(prices)
+        
+        if rsi[-1] < self.low:
+            # buy
+            # score_plus = abs(np.log((self.low -rsi[-1])/self.low) / 4)
+            score_plus = 0
+            # print((1 + score_plus) * self.weight)
+            return (1 + score_plus) * self.weight
+        elif rsi[-1] > self.high:
+            # sell
+            # score_plus = abs(np.log(((100-self.high) - rsi[-1])/(100-self.high))/ 4)
+            score_plus = 0
+            return (-1 - score_plus) * self.weight
+        return 0
+
+
 def rsi(prices):
     """RSI implementation. Pass in prices to return RSI calulations.
 
