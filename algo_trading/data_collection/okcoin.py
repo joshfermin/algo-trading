2e6174e45681b6218d6c71efa491daa7b578ac5a
joshfermin <joshfermin@gmail.com>
2021-02-05 17:17:16 -0700

    Add script to record okcoin data for ETH

--- algo_trading/data_collection/okcoin.py
+++ algo_trading/data_collection/okcoin.py
@@ -6,26 +6,39 @@ from datetime import datetime
 from pymongo import MongoClient
 from apscheduler.schedulers.blocking import BlockingScheduler
 from algo_trading.mongo_connect import historical_data_collection
+from pymongo import errors
 
-collection = historical_data_collection()
-
-
-def tick():
+def record_data(symbol, instrument_id, granularity_in_seconds, granularity_name):
+    collection = historical_data_collection(symbol, granularity_name)
     """Gather market data from OKCoin Spot Price API and insert them into a
        MongoDB collection."""
-    ticker = requests.get('https://www.okcoin.com/api/spot/v3/instruments/BTC-USD/ticker').json()
-    depth = requests.get('https://www.okcoin.com/api/spot/v3/instruments/BTC-USD/book?size=60').json()
-    date = dateutil.parser.isoparse(ticker['timestamp'])
-    price = float(ticker['last'])
-    v_bid = sum([float(bid[1]) for bid in depth['bids']])
-    v_ask = sum([float(ask[1]) for ask in depth['asks']])
-    collection.insert_one({'date': date, 'price': price, 'v_bid': v_bid, 'v_ask': v_ask})
-    print(date, price, v_bid, v_ask)
+
+    candles = requests.get(f'https://www.okcoin.com/api/spot/v3/instruments/{instrument_id}/candles?granularity={granularity_in_seconds}').json()
+    documents = []
+    for candle in candles:
+        documents.append({
+            "date": dateutil.parser.isoparse(candle[0]),
+            "open": float(candle[1]),
+            "high": float(candle[2]),
+            "low": float(candle[3]),
+            "close": float(candle[4]),
+            "volume": float(candle[5]),
+        })
+    try:
+        # inserts new documents even on error
+        collection.insert_many(documents)
+    except errors.BulkWriteError as e:
+        panic_list = list(filter(lambda x: x['code'] != 11000, e.details['writeErrors']))
+        if len(panic_list) > 0:
+            print(f"these are not duplicate errors {panic_list}")
 
 def main():
     """Run tick() at the interval of every ten seconds."""
     scheduler = BlockingScheduler(timezone=utc)
-    scheduler.add_job(tick, 'interval', seconds=10)
+    scheduler.add_job(record_data, 'cron', ['ETH', 'ETH-USD', 86400, 'day'], day="*")
+    scheduler.add_job(record_data, 'cron', ['ETH', 'ETH-USD', 3600, 'hour'], hour="*")
+    scheduler.add_job(record_data, 'cron', ['ETH', 'ETH-USD', 300, 'five_minutes'], minute="*/5")
+    scheduler.add_job(record_data, 'cron', ['ETH', 'ETH-USD', 60, 'one_minute'], minute="*")
     try:
         scheduler.start()
     except (KeyboardInterrupt, SystemExit):
