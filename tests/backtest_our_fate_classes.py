22137c084785a8ff2c952548a58a4fab0e1c0831
Edward Zhu <edward.r.zhu@gmail.com>
2021-02-07 19:56:30 -0700

    time utils function added

--- tests/backtest_our_fate_classes.py
+++ tests/backtest_our_fate_classes.py
@@ -11,28 +11,31 @@ from backtesting.test import SMA, GOOG
 # imports for strategy, that may be removed when absracted out
 from algo_trading.strategies.parabolic_sar import parabolic_sar, ParabolicSAR
 from algo_trading.strategies.rsi import rsi, RSI
+from algo_trading.strategies.stop_loss import STOP_LOSS
 from algo_trading.strategies.sma import SMA_CROSSOVER_TREND, SMA_CROSSOVER_EVENT
 from algo_trading.exchange.exchange_context import ExchangeContext
 from algo_trading.exchange.robinhood_actions import RobinhoodActions
+from utils.time import handle_time_period
 
 # 
 # TODO: move this test strat out of her to make it more robust in taking an strats
 # 
-class PSAR_RSI(Strategy):
+class THE_VERSION_WE_CALL_ONE(Strategy):
 
     def init(self, position = 0):
         close = self.data.Close
         high = self.data.High
         low = self.data.Low
 
-        self.p_sar = ParabolicSAR(0, 0.01, 0.2)
+        self.p_sar = ParabolicSAR(1, 0.02, 0.2)
         self.rsi = RSI(0, 50, 50)
-        self.sma = SMA_CROSSOVER_EVENT(1, 300, 100)
+        self.sma = SMA_CROSSOVER_TREND(1, 48, 24)
+        self.stop_loss = STOP_LOSS(1, -0.05)
 
         self.indicator_p_sar = self.I(self.p_sar.calcParabolicSAR, high, low)
         self.indicator_rsi = self.I(self.rsi.calcRSI, close)
-        self.indicator_sma_long = self.I(self.sma.calcSMA, close, 200)
-        self.indicator_sma_short = self.I(self.sma.calcSMA, close, 50)
+        self.indicator_sma_long = self.I(self.sma.calcSMA, close, 48)
+        self.indicator_sma_short = self.I(self.sma.calcSMA, close, 24)
 
     def next(self):
         current_bid_price = self.data.Close[-1]
@@ -41,36 +44,35 @@ class PSAR_RSI(Strategy):
             p_sar_score = self.p_sar.getScore(current_bid_price, self.data.High, self.data.Low)
             rsi_score = self.rsi.getScore(self.data.Close)
             sma_score = self.sma.getScore(self.data.Close)
+            stop_loss_score = self.stop_loss.getScore(self.position.pl_pct)
 
             scores = np.array([p_sar_score, rsi_score, sma_score])
             average_score = statistics.mean(scores[scores != 0])
 
             if self.position.size <= 0 and average_score >= 1:
                 self.buy()
-            elif self.position.size > 0 and average_score <= -1:
+            elif self.position.size > 0 and (average_score <= -1 or stop_loss_score == -1):
                 self.position.close()
 
 
             relative_position = self.position.size if self.position.size >= 0 else 0
-            print(  "total:", round(self.equity),
-                    "units:", round(relative_position, 2),
-                    # "sar:", round(latest_sar, 2),
-                    # "rsi:", round(latest_rsi,2),
-                    "bid_price:", round(current_bid_price, 2))
+            # print(  "total:", round(self.equity),
+            #         "units:", round(relative_position, 2),
+            #         # "sar:", round(latest_sar, 2),
+            #         # "rsi:", round(latest_rsi,2),
+            #         "bid_price:", round(current_bid_price, 2))
 
 
 def backtest_our_fate(strat, ticker, cash):
+    start_date = None
+    end_date = None
     exchange_actions = ExchangeContext(RobinhoodActions())
     crypto_historicals = exchange_actions.get_crypto_historicals(ticker, interval="hour", span="3month", bounds="24_7")
 
-    # handle start and end day to find the corresponding indexes in historical crypto data
-    start_date = datetime.datetime(2020, 12, 2).strftime("%Y-%m-%dT%H:%M:%SZ")
-    end_date = datetime.datetime(2021, 1, 30).strftime("%Y-%m-%dT%H:%M:%SZ")
-    start_date_index = [historical['begins_at'] for historical in crypto_historicals].index(start_date)
-    end_date_index = [historical['begins_at'] for historical in crypto_historicals].index(end_date)
+    # start_date = datetime.datetime(2021, 1, 2)
+    # end_date = datetime.datetime(2021, 2, 6)
 
-    # 336 hours is 14 days, we are taking 2 weeks af data before the first buy, and ending on the end day index
-    historical_period = crypto_historicals[start_date_index-336:end_date_index]
+    historical_period = handle_time_period(crypto_historicals, start_date, end_date)
 
     # get all relevant prices from the historical period above
     close_prices = np.asarray([float(historical['close_price']) for historical in historical_period])
@@ -102,5 +104,5 @@ def backtest_our_fate(strat, ticker, cash):
     bt.plot(filename="tests/plots/HEREWEGO.html")
 
 
-backtest_our_fate(PSAR_RSI, "ETH", 40000)
+backtest_our_fate(THE_VERSION_WE_CALL_ONE, "ETH", 40000)
 
---
+++ utils/time.py
@@ -0,0 +1,18 @@
+# 
+# move to a utils function
+# 
+def handle_time_period(total_historicals, start_date=None, end_date=None):
+    start_index = 336
+    end_index = len(total_historicals)
+
+    # handle start and end day to find the corresponding indexes in historical crypto data
+    if start_date:
+        start_date_str = start_date.strftime("%Y-%m-%dT%H:%M:%SZ")
+        start_date_index = [historical['begins_at'] for historical in total_historicals].index(start_date_str)
+        start_index = start_date_index - 336
+
+    if end_date:
+        end_date_str = end_date.strftime("%Y-%m-%dT%H:%M:%SZ")
+        end_index = [historical['begins_at'] for historical in total_historicals].index(end_date_str)
+
+    return total_historicals[start_index:end_index]
