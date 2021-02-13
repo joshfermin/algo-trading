8a4c615c3f8b1bf27df81a28e59bd5ca1e67b419
joshfermin <joshfermin@gmail.com>
2021-02-13 01:13:40 -0700

    convert the last strat over lets get it

--- algo_trading/strategies/rsi.py
+++ algo_trading/strategies/rsi.py
@@ -2,9 +2,9 @@ import time
 import talib
 import numpy as np
 
+from algo_trading.enums import Decision
 class RSI:
-    def __init__(self, weight, high=70, low=30):
-        self.weight = weight
+    def __init__(self, high=70, low=30):
         self.high = high
         self.low = low
 
@@ -14,46 +14,19 @@ class RSI:
     Returns:
         A numpy array. Each element represents the parabolicSAR calculation
     """
-    def calcRSI(self, prices):
-        return talib.RSI(prices)
+    def calculate(self, close_prices):
+        return talib.RSI(close_prices)
 
-    def getScore(self, prices):
-        rsi = self.calcRSI(prices)
+    def indicators(self):
+        return [{"name": "rsi"}]
+
+    def getDecision(self, params):
+        rsi = self.calculate(params['close'])
         
         if rsi[-1] < self.low:
-            # buy
-            # score_plus = abs(np.log((self.low -rsi[-1])/self.low) / 4)
-            score_plus = 0
-            # print((1 + score_plus) * self.weight)
-            return (1 + score_plus) * self.weight
+            return Decision.BUY
         elif rsi[-1] > self.high:
-            # sell
-            # score_plus = abs(np.log(((100-self.high) - rsi[-1])/(100-self.high))/ 4)
-            score_plus = 0
-            return (-1 - score_plus) * self.weight
-        return 0
+            return Decision.SELL
+        return Decision.NOOP
 
-
-def rsi(prices):
-    """RSI implementation. Pass in prices to return RSI calulations.
-
-    Args:
-        prices: A numpy array of floats representing prices over a time period.
-
-    Returns:
-        A numpy array. Each element represents the parabolicSAR calculation
-    """
-    return talib.RSI(prices)
-
-# def evaluate_performance(latest_sar, current_bid_price, positions, position, bank_balance):
-    # if(position == 0 and current_bid_price > latest_sar):
-    #     position += positions
-    #     bank_balance -= current_bid_price*positions
-    # if(position > 0 and current_bid_price < latest_sar):
-    #     position -= positions
-    #     bank_balance += current_bid_price*positions
-    # print("position:", position, "bank_balance:", bank_balance, "sar:", latest_sar, "bid_price:", current_bid_price)
-    # return {
-    #     "position": position,
-    #     "bank_balance": bank_balance
-    # }
+rsi = RSI
